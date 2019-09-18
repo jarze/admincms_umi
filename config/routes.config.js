51d6880d09828849548b1aec76425fbc80f00f55
ranxx <ranxx@chinawyny.com>
2019-09-18 17:56:50 +0800

    [fix] 路由匹配更新

--- config/routes.config.js
+++ config/routes.config.js
@@ -5,17 +5,17 @@ export default [
 		routes: [
 			{
 				path: '*/:menuId/list',
-				component: './comm/list/index.js',
+				component: './*/$menuId/list/index.js',
 				Routes: ['./src/routes/list.js'],
 			},
 			{
-				path: '*/:menuId/list/page/:id?',
-				component: './comm/infoPage/index.js',
+				path: '*/:menuId/list/page/:id',
+				component: './*/$menuId/list/page/$id.js',
 				Routes: ['./src/routes/list.js'],
 			},
 			{
 				path: '*/:menuId/list/edit/:id?',
-				component: './comm/formPage/index.js',
+				component: './*/$menuId/list/edit/$id$.js',
 				Routes: ['./src/routes/list.js'],
 			},
 		],
