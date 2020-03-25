c1275e76d1a32fed6ad04e179205335694db8b1f
ranxx <ranxx@chinawyny.com>
2020-03-25 16:39:47 +0800

    Form 优化

--- src/components/comm/Form/index.js
+++ src/components/comm/Form/index.js
@@ -16,25 +16,21 @@ const getColsHandle = count => {
 	Object.keys(cols).forEach(key => {
 		submitCols[key] = handleColumns(cols[key])
 	})
-	return { cols, submitCols }
+	return { cols, submitCols };
 }
 
 const getColWap = (type, count, col, submitCol) => {
 	switch (type) {
+		case 'col': {
+			let { cols = { span: 8 }, submitCols = { span: 24 } } = col ? { cols: { span: col }, submitCols: { span: col } } : getColsHandle(count);
+			if (submitCol) submitCols = { span: submitCol };
+			const FormContentWap = props => <Row gutter={16} {...props} />;
+			const FormItemWap = props => <Col {...cols} {...props} />;
+			const ForSubmitItemWap = props => <Col {...submitCols} {...props} />;
+			return [FormContentWap, FormItemWap, ForSubmitItemWap];
+		};
 		default: {
-			let { cols = { span: 8 }, submitCols = { span: 24 } } = col
-				? { cols: { span: col }, submitCols: { span: col } }
-				: getColsHandle(count)
-			if (submitCol) {
-				submitCols = { span: submitCol }
-			}
-			const FormContentWap = props =>
-				type === 'col' ? <Row gutter={16} {...props} /> : <Fragment {...props} />
-			const FormItemWap = props =>
-				type === 'col' ? <Col {...cols} {...props} /> : <Fragment {...props} />
-			const ForSubmitItemWap = props =>
-				type === 'col' ? <Col {...submitCols} {...props} /> : <Fragment {...props} />
-			return [FormContentWap, FormItemWap, ForSubmitItemWap]
+			return [Fragment, Fragment, Fragment];
 		}
 	}
 }
@@ -49,22 +45,17 @@ export const CForm = ({
 	okText = '确定',
 	cancelText = '取消',
 	type = 'col', // 'follow' | 'col' | 'center',
-	onValuesChange,
 	col, // 固定排版 24 8,type = 'col'时有效
-	submitCol,
+	submitCol,// 提交按钮固定排版, type = 'col'时有效
+	onValuesChange,
 	...formProps
 }) => {
-	const { getFieldDecorator, validateFields, resetFields } = form
+	const { getFieldDecorator, validateFields, resetFields } = form;
 	useEffect(() => {
-		if (data) {
-			resetFields()
-		}
+		if (data) resetFields();
 	}, [data])
 
-	const [FormContentWap, FormItemWap, ForSubmitItemWap] = useMemo(
-		() => getColWap(type, items.length, col, submitCol),
-		[items.length, type]
-	)
+	const [FormContentWap, FormItemWap, ForSubmitItemWap] = useMemo(() => getColWap(type, items.length, col, submitCol), [items.length, type])
 
 	const handleSubmit = e => {
 		e.preventDefault()
@@ -81,48 +72,34 @@ export const CForm = ({
 		onReset && onReset({})
 	}
 
-	const formContent = items.map(
-		(
-			{
-				cols,
-				key,
-				label,
-				placeholder,
-				options,
-				render,
-				defaultValue,
-				disabled = false,
-				...itemProps
-			},
-			index
-		) => {
-			if (render === null || (render && render(form, data) === null)) return null
-			return (
-				<FormItemWap key={key || index} {...cols}>
-					{key ? (
-						<FormItem label={label} key={key} {...itemProps}>
-							{getFieldDecorator(key, {
-								initialValue: data && data[key] !== undefined ? data[key] : defaultValue,
-								...options,
-							})(
-								render ? (
-									render(form)
-								) : (
-										<Input
-											type="text"
-											disabled={disabled}
-											placeholder={placeholder || (label ? `输入${label}` : '')}
-										/>
-									)
-							)}
-						</FormItem>
-					) : (
-							render && render(form, data)
-						) // 不指定key， 直接渲染render内容
-					}
-				</FormItemWap>
-			)
-		}
+	const formContent = items.map(({
+		cols,
+		key,
+		label,
+		placeholder,
+		options,
+		render,
+		defaultValue,
+		disabled = false,
+		...itemProps
+	}, index) => {
+		if (render === null || (render && render(form, data) === null)) return null
+		return (
+			<FormItemWap key={key || index} {...cols}>
+				{key ? (
+					<FormItem label={label} key={key} {...itemProps}>
+						{getFieldDecorator(key, {
+							initialValue: data && data[key] !== undefined ? data[key] : defaultValue,
+							...options,
+						})(
+							render ? render(form) : <Input type="text" disabled={disabled} placeholder={placeholder || (label ? `输入${label}` : '')} />
+						)}
+					</FormItem>
+				) : (render && render(form, data)) // 不指定key， 直接渲染render内容
+				}
+			</FormItemWap>
+		)
+	}
 	)
 
 	const formSubmit = (onSubmit || onReset) && (
@@ -135,26 +112,18 @@ export const CForm = ({
 						: {}
 				}
 			>
-				{onSubmit && (
-					<Button type="primary" htmlType="submit" loading={loading}>
-						{okText}
-					</Button>
-				)}
+				{onSubmit && <Button type="primary" htmlType="submit" loading={loading}>{okText}</Button>}
 				{onReset && (
 					<>
 						{onSubmit && <Divider type="vertical" />}
-						<Button type="cancel" onClick={handleClear}>
-							{cancelText}
-						</Button>
+						<Button type="cancel" onClick={handleClear}>{cancelText}</Button>
 					</>
 				)}
 			</FormItem>
 		</ForSubmitItemWap>
 	)
 
-	if (items.length === 0) {
-		return <Fragment />
-	}
+	if (items.length === 0) return <Fragment />
 
 	return (
 		<Form
