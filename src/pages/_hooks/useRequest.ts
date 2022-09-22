10c279d4783a00c63bafb57a38a5a9164d4c95d6
jarze <1107318502@qq.com>
2022-09-22 15:51:05 +0800

    update useRequest

--- src/pages/_hooks/useRequest.ts
+++ src/pages/_hooks/useRequest.ts
@@ -1,32 +1,43 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react'
+
+interface UseRequestProps<T> {
+  /* 发起请求 */
+  fetchData?: (params?: any) => Promise<T | any>
+  /* 请求参数， 改变会发起请求 */
+  params?: any
+  handlerData?: (/* 请求返回结果 */ res?: any) => T
+}
 
 export default function useRequest<T extends any>({
   fetchData,
   params = null,
-  handlerData = null,
-}) {
-  const [loading, setLoading] = useState(false);
-  const mounted = useRef<boolean>(true);
-  const [data, setData] = useState<T>();
+  handlerData = null
+}: UseRequestProps<T>) {
+  const [loading, setLoading] = useState(false)
+  const mounted = useRef<boolean>(true)
+  const [data, setData] = useState<T>()
 
   useEffect(
     () => () => {
-      mounted.current = false;
+      mounted.current = false
     },
-    [],
-  );
+    []
+  )
 
   useEffect(() => {
-    if (!fetchData) return;
-    setLoading(true);
+    if (!fetchData) return
+    setLoading(true)
     fetchData(params)
       .then(res => {
-        mounted.current && setData(handlerData ? handlerData(res) : res);
+        mounted.current && setData(handlerData ? handlerData(res) : res)
+      })
+      .catch(e => {
+        console.error(e)
       })
       .finally(() => {
-        mounted.current && setLoading(false);
-      });
-  }, [fetchData, params]);
+        mounted.current && setLoading(false)
+      })
+  }, [fetchData, params])
 
-  return { data, loading, setData };
+  return { data, loading, setData }
 }
